c474a86908c2d7444bcf25923858c2932a94b455
Mihnea Neagu <mihneaneagu709@yahoo.com>
2021-09-29 19:55:49 +0300

    Primul comit

--- main.py
+++ main.py
@@ -2,32 +2,54 @@
 Returneaza true daca n este prim si false daca nu.
 '''
 def is_prime(n):
-  # codul vostru aici
+  a=5
+  if a<2:
+    return False
+  elif a==2:
+    return True
+  else:
+    ok=True
+    for i in range(3,a/2+1,2):
+      if a%i==0:
+        ok=False
+      if ok==1:
+        return True
+      else:
+        return False
+
+    
+
+
+  
   
   
 '''
 Returneaza produsul numerelor din lista lst.
 '''
 def get_product(lst):
-  # codul vostru aici
+  p1=0
+  for val in lst:
+    p1*=val
+
+  print (p1)
   
   
 '''
 Returneaza CMMDC a doua numere x si y folosind primul algoritm.
 '''
 def get_cmmdc_v1(x, y):
-  # codul vostru aici
+  return True
   
   
 '''
 Returneaza CMMDC a doua numere x si y folosind al doilea algoritm.
 '''
 def get_cmmdc_v2(x, y):
-  # codul vostru aici
+  return True
   
   
 def main():
-  # interfata de tip consola aici
+  return True
 
 if __name__ == '__main__':
   main()
